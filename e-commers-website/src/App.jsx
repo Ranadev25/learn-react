e1b98e3b418eadd3f40ed4632e5c572a32d4055a
Ranadev25 <ranakhandev2025@gmail.com>
2026-03-15 12:52:28 +0600

    complate setup project

---
+++ e-commers-website/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='flex items-center justify-center min-h-full text-red-600'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
