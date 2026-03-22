6dcd2029acd8a5d408da12e504f283052e5f7cda
Ranadev25 <ranakhandev2025@gmail.com>
2026-03-22 09:03:07 +0600

    created dropdown menu 2

---
+++ e-commers-website/src/app/_components/Dropdownmenu.tsx
@@ -0,0 +1,14 @@
+// import Link from "next/link";
+// import React, { useRef } from "react";
+
+// const Dropdownmenu = () => {
+
+ 
+  
+
+//   return (
+    
+//   );
+// };
+
+// export default Dropdownmenu;
--- e-commers-website/src/app/_components/NavberTop.tsx
+++ e-commers-website/src/app/_components/NavberTop.tsx
@@ -7,35 +7,56 @@ import QrCode from "../../../public/image/home/photo/image.png";
 import APP_Stor from "../../../public/image/home/cardBTN/abailable-app-store.png";
 import Google_pay from "../../../public/image/home/cardBTN/googlePayBlackBTN.png";
 
+import { GrUserManager } from "react-icons/gr";
+import { MdContactPhone } from "react-icons/md";
+import { FaCaravan } from "react-icons/fa";
+import { FaBoxOpen } from "react-icons/fa6";
+import { GiCash } from "react-icons/gi";
+import { LuBadgeDollarSign } from "react-icons/lu";
+
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
   const qrRef = useRef<HTMLDivElement>(null);
 
-  const handleToggle = () => {
-    setIsOpen(prev => !prev);
-  };
+  const openmenu = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (!isOpen) return;
-      if (qrRef.current && qrRef.current.contains(event.target as Node)) {
-        return;
+    const handleClickOutside = (e) => {
+      if (!qrRef.current) {
+        setIsOpen(false);
       }
       setIsOpen(false);
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, [isOpen]);
 
+   useEffect(() => {
+    const handleClickoutside = (e) => {
+      if (!openmenu.current) {
+        setOpenMenu(false);
+      }
+      setOpenMenu(false);
+    };
+
+    document.addEventListener("click", handleClickoutside);
+    return () => {
+      document.removeEventListener("click", handleClickoutside);
+    };
+  }, [openMenu]);
 
   return (
     <div className="">
       <div className="">
         <ul className="flex justify-end gap-2 sm:gap-5 md:gap-14 font-medium text-[11.5px] text-[#FFFFFF] py-1 relative">
-          <li className="cursor-pointer relative" onClick={handleToggle}>
+          <li
+            className="cursor-pointer relative"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <Link href="#">SAVE MORE ON APP</Link>
             <div
               ref={qrRef}
@@ -53,12 +74,59 @@ const Menu = () => {
               </div>
             </div>
           </li>
+
           <li className="cursor-pointer">
             <a>BECOME A SELLER</a>
           </li>
-          <li className="cursor-pointer">
-            <a>HELP & SUPPORT</a>
+
+          <li className="cursor-pointer relative" onClick={() => setOpenMenu(!openMenu)}>
+            <a className="">HELP & SUPPORT</a>
+            {openMenu ? (
+              <div ref={openmenu} className="absolute top-8.5 left-0 z-10 bg-white text-gray-500 min-w-54 p-3">
+                <ul>
+                  <li >
+                    <Link href="" className="flex gap-3 text-2xl items-center my-2" >
+                      <GrUserManager />
+                      <h1 className="text-[16px]">Help Center</h1>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="" className="flex gap-3 text-2xl items-center my-4">
+                      <MdContactPhone />
+                      <h1 className="text-[16px]">Contact Customer Care</h1>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="" className="flex gap-3 text-2xl items-center my-4">
+                      <FaCaravan/>
+                      <h1 className="text-[16px]">Shipping & Delivery</h1>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="" className="flex gap-3 text-2xl items-center my-4">
+                      <FaBoxOpen/>
+                      <h1 className="text-[16px]">Order</h1>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="" className="flex gap-3 text-2xl items-center my-4">
+                      <GiCash/>
+                      <h1 className="text-[16px]">Payment</h1>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="" className="flex gap-3 text-2xl items-center mt-4">
+                      <LuBadgeDollarSign/>
+                      <h1 className="text-[16px]">Returns & Refunds</h1>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              ""
+            )}
           </li>
+
           <li className="cursor-pointer">
             <a>LOGIN</a>
           </li>
